b0b4fed86efda4d0a2fa5ca313f3e9cbf4dd3a97
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-06-11 15:05:22 -0700

    Add try catch loops for getting canonical kmers

--- src/PositionsKmerDistributions.cpp
+++ src/PositionsKmerDistributions.cpp
@@ -128,10 +128,18 @@ void get_kmer_distributions_by_position(const string &positions_file_path,
       kmers.insert(canonical_kmers.begin(), canonical_kmers.end());
 
       for (auto &k: kmers){
-        pos_hist = pos.get_pos_kmer(k)->get_hist(min, max, size, min_prob_threshold);
-        data[line.contig+"_"+line.strand+"_"+to_string(line.position-i)+"_"+ k] = pos_hist;
-        kmer_hist = edh.get_kmer(k).get_hist(min, max, size, min_prob_threshold);
-        data[k] = kmer_hist;
+        try {
+          pos_hist = pos.get_pos_kmer(k)->get_hist(min, max, size, min_prob_threshold);
+          data[line.contig+"_"+line.strand+"_"+to_string(line.position-i)+"_"+ k] = pos_hist;
+        } catch (AssertionFailureException) {
+          continue;
+        };
+        try {
+          kmer_hist = edh.get_kmer(k).get_hist(min, max, size, min_prob_threshold);
+          data[k] = kmer_hist;
+        } catch (AssertionFailureException) {
+            continue;
+        };
       }
       pos_file = pos_specific_dir / path(line.contig+"_"+line.strand+"_"+to_string(line.position-i)+".csv");
       write_plot_kmer_dist_file(pos_file, min, max, size, min_prob_threshold, data);
